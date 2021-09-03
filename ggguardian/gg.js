c11010c4d3cdf447b15b5d86bccd0eee0f0d9d2a
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-09-02 20:51:04 -0700

    done

---
+++ ggguardian/gg.js
@@ -0,0 +1,13 @@
+
+// Put all the javascript code here, that you want to execute after page load.
+let blockedURL = decodeURI(new URL(window.location.href).searchParams.get('rdr'))
+let teacher = JSON.parse(decodeURI(new URL(window.location.href).searchParams.get('cs')))[0].name
+let teacherFirst  = String(teacher).split(' ')[0]
+
+console.log(blockedURL)
+console.log(teacher)
+console.log(teacherFirst)
+
+
+
+document.getElementById('content').innerHTML = `<h1>Hey there!</h1><p>${teacher} thinks its funny.... its not</p><iframe src="https://maxw2.github.io/chrome_dino_js/" width="570px" height="180px" style="position:relative;left:-20%;"></iframe>`  
