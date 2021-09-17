77715abb7c1a6a02f8ad7d84d0afe24d18e53607
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-09-16 21:51:34 -0700

    fix

--- schoolloop/redesign/dashboard/dashboard.js
+++ schoolloop/redesign/dashboard/dashboard.js
@@ -2,14 +2,38 @@
 
 (async function() {
     'use strict'
+    /*
+        try {
 
-    var chrome = browser || chrome
+            
 
-    let user = await chrome.storage.local.get("user")
-    user = user.user
-    var first = String(user.fullName).split(', ')[1]
-    var last = String(user.fullName).split(', ')[0]
-    document.getElementById('user').innerHTML = `${String(user.fullName).split(', ')[1]} ${String(user.fullName).split(', ')[0]}`
+        } catch (e) {
+            try {
+
+                let user = await browser.storage.local.get("user")
+
+            } catch (e) {
+
+                console.log(e)
+
+            }
+
+
+        }
+        */
+    let user = await chrome.storage.local.get()
+    try {
+        let user = await chrome.storage.local.get()
+        user = user.user
+        var first = String(user.fullName).split(', ')[1]
+        var last = String(user.fullName).split(', ')[0]
+        document.getElementById('user').innerHTML = `${String(user.fullName).split(', ')[1]} ${String(user.fullName).split(', ')[0]}`
+    } catch (e) {
+        //Not logged in
+        //document.location.href = document.location.origin + 'schoolloop/redesign/login/login.html'
+        console.log(e)
+    }
+
 
 
 
--- schoolloop/redesign/login/login.js
+++ schoolloop/redesign/login/login.js
@@ -1,50 +1,3 @@
-//document.getElementById("submit").addEventListener("click", validate);
-const QueryString = window.location.search;
-const urlParams = new URLSearchParams(QueryString);
-chrome.storage.local.clear()
-    //if (browser.storage.local.get("user") || browser.storage.local.get("user")) document.location.href = document.location.origin + "/schoolloop/redesign/dashboard/dashboard.html"
-
-if (urlParams.get('user') && urlParams.get('pass')) {
-    document.getElementsByClassName('form-signin').innerHTML = `<div class="d-flex justify-content-center"><div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div></div>`
-    const user = urlParams.get('user')
-    const pass = urlParams.get('pass')
-    console.log(`Trying to login with key: ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}`)
-                fetch(`https://hmbhs.schoolloop.com/mapi/login?version=3&devToken=${encodeURI(chrome.runtime.id)}&devOS=${encodeURI(chrome.runtime.getManifest().version)}&year=${new Date().getFullYear()}`, {
-                            headers: {
-                                authorization: `Basic ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}`
-            }
-                }).then((data) => {
-                        console.log('Login successful')
-                    data.json().then((data) => {
-                    
-                    var user = data
-						user.auth = `Basic ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}` //SAve Auth header for future use
-                        browser.storage.local.set({ user }).then(() => { //Save it in extension storage since cokkies/window storage gets cleared every time the extension page is loaded
-                            console.log('saved')
-                        })
-                        document.location = document.location.origin + "/schoolloop/redesign/dashboard/dashboard.html"
-                    
-                    
-                    })
-                        
-                        
-                    })
-                    .catch((err) => {
-					//Want to do some bootstrap magic here in the future
-					console.log('Login Failed')
-                    document.location = document.location.origin + document.location.pathname + `?failed=true`
-                })
-}
-if (urlParams.get('failed')) {
-
-    console.log('Showing Error Toast')
-    var toastLiveExample = document.getElementById('liveToast')
-    var toast = new bootstrap.Toast(toastLiveExample)
-    toast.show()
-
-}
-
-
 function validate(e) {
     e.preventDefault();
 
@@ -55,24 +8,102 @@ function validate(e) {
 
     return valid;
 }
+
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return null;
 }
+
+function login(p = null) {
+
+    if (p) document.location.href = document.location.origin + `/schoolloop/redesign/dashboard/dashboard.html?user=${p}`
+    document.location.href = document.location.origin + "/schoolloop/redesign/dashboard/dashboard.html"
+
+
+}
+//document.getElementById("submit").addEventListener("click", validate);
+const QueryString = window.location.search;
+const urlParams = new URLSearchParams(QueryString);
+//chrome.storage.local.clear()
+
+//Cookie checker
+/*
+try {
+    if (browser.storage.local.get("user")) login()
+} catch {
+    try {
+        if (chrome.storage.local.get("user")) login()
+    } catch {
+        try {
+            if (getCookie('user')) login()
+        } catch {
+            console.warn('No User Detected')
+        }
+    }
+}
+*/
+async function main() {
+    if (urlParams.get('user') && urlParams.get('pass')) {
+        document.getElementsByClassName('form-signin').innerHTML = `<div class="d-flex justify-content-center"><div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div></div>`
+        const user = urlParams.get('user')
+        const pass = urlParams.get('pass')
+        console.log(`Trying to login with key: ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}`)
+    let response = await fetch(`https://hmbhs.schoolloop.com/mapi/login?version=3&devToken=${encodeURI(chrome.runtime.id)}&devOS=${encodeURI(chrome.runtime.getManifest().version)}&year=${new Date().getFullYear()}`, {
+        headers: {
+                                authorization: `Basic ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}`
+        }
+    })
+    response = await response.json()
+    response.auth = `Basic ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}` //SAve Auth header for future use
+    console.log(response)
+    await chrome.storage.local.set({ response }, function () {
+        console.log("Data was saved.");
+        login()
+    });
+                try {
+                    console.log('false')
+                } catch (e) {
+                            try {
+                                browser.storage.local.set({ response }).then(() => { //Save it in Web Extension extension storage since cookies/window storage gets cleared every time the extension page is loaded
+                                    console.log('saved')
+                                })
+                            } catch (e) {
+                                try {
+                                    document.cookie = `user=${String(response)}`
+                                } catch(e) {
+                                    console.warn('saving disabled, enabling crosspage url params mode')
+                                    login(String(response))
+                                }
+                            }
+                        }
+                        
+                //login()
+}
+if (urlParams.get('failed')) {
+
+    console.log('Showing Error Toast')
+    var toastLiveExample = document.getElementById('liveToast')
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+
+    }
+}
+main()
