dd03600d97e084cf447aaae29d6fdd8560f81f60
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-09-30 19:58:03 -0700

    a

--- searchEngineRedirect/redirect.js
+++ searchEngineRedirect/redirect.js
@@ -7,4 +7,4 @@ const query = urlParams.get('q')
 
 if (query.includes('schoolloop') || query.includes('school loop')) location.href = 'https://hmbhs.schoolloop.com'
 if (query.includes('google classroom')) location.href = 'https://classroom.google.com'
-if (query.includes('bell schedule ')) location.href = 'https://hmbhs.cabrillo.k12.ca.us/our_school/bell_schedules'
+if (query.includes('bell+schedule ')) location.href = 'https://hmbhs.cabrillo.k12.ca.us/our_school/bell_schedules'
