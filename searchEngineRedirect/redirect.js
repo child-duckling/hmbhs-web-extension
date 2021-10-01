5db54417853303d29c29b4d96adddec4abe3b9ea
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-10-01 11:34:03 -0700

    some goog goog stuff

--- searchEngineRedirect/redirect.js
+++ searchEngineRedirect/redirect.js
@@ -5,6 +5,20 @@ const urlParams = new URLSearchParams(QueryString);
 const query = urlParams.get('q')
 
 
-if (query.includes('schoolloop') || query.includes('school loop')) location.href = 'https://hmbhs.schoolloop.com'
-if (query.includes('google classroom')) location.href = 'https://classroom.google.com'
-if (query.includes('bell+schedule ')) location.href = 'https://hmbhs.cabrillo.k12.ca.us/our_school/bell_schedules'
+
+//HMBHS
+if (query.includes('hmbhs')) {
+  if (query.includes('schoolloop') || query.includes('school loop')) location.href = 'https://hmbhs.schoolloop.com'
+  if (query.includes('bell')) location.href = 'https://hmbhs.cabrillo.k12.ca.us/our_school/bell_schedules'
+  if (query.includes('site')) location.href = 'https://hmbhs.cabrillo.k12.ca.us'   
+}
+
+//Google Stuff
+if (query.includes('google')) {
+  if (query.includes('classroom')) location.href = 'https://classroom.google.com'
+  if (query.includes('slides')) location.href = 'https://slides.google.com'
+  if (query.includes('docs')) location.href = 'https://docs.google.com'
+  if (query.includes('drive')) location.href = 'https://drive.google.com'
+}
+
+if (query.includes('gmail')) location.href = 'https://mail.google.com'
